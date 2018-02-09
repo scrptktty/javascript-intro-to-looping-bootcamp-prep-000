022375243a085f59ac1f3a944fb8f444183ae771
Kachel Wittig <kjwittig@gmail.com>
2018-02-09 07:30:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 26; i++) {
-  i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+    var mySentence = `I am ${i} stange loop${i === 1 ? "" : s}.`
+    array.push(mySentence)
   }
-  `I am ${i} stange loop${i === 1 ? "" : s}.` 
+  
   return array
 }
 
