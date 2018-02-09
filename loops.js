79fffdbc355dc9835d4059b1e91abccb259de43d
Kachel Wittig <kjwittig@gmail.com>
2018-02-09 07:18:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
-  for (let i= 0; i < 26; i++) {
-    console.log("I am ${i} strange loops.")
+  for (let i = 0; i < 26; i++) {
+  array.push("I am ${i} strange loops.")
   }
   return array
 }
